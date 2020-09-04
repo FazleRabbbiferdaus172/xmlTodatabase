1fe7186b59b357c1523c139e7533785ff6ad8acd
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-09-05 02:46:13 +0600

    Add files via upload

---
+++ database_To_xml.py
@@ -0,0 +1,50 @@
+from lxml import etree
+from lxml.builder import E as buildE
+import mysql.connector
+from configcon import conn
+
+# ***************XMl builder***************
+
+
+def E(tag, parent=None, content=None):
+    element = buildE(tag)
+    if content is not None:
+        element.text = content
+    if parent is not None:
+        parent.append(element)
+    return element
+
+
+def fetchXML(db_name, tbl_name, fields, rows):
+    #fields = [x[0] for x in cursor.description]
+    doc = E(db_name)  # database name
+    for record in rows:  # rows
+        r = E(tbl_name, parent=doc)  # table name
+        for (k, v) in zip(fields, record):  # cols,row
+            E(k, content=v, parent=r)
+    return doc
+
+
+# *********************connecting database************************
+db = conn()
+print(db)
+database_name = 'catalog'
+table_name = 'book'
+my_cursor = db.cursor()
+query1 = "SHOW COLUMNS FROM book"
+query2 = "select * from book"
+my_cursor.execute(query1)
+dummy = my_cursor.fetchall()
+cols = []
+for i in dummy:
+    cols.append(i[0])
+my_cursor.execute(query2)
+rows = my_cursor.fetchall()
+# print(cols)
+# print(rows)
+doc = fetchXML(database_name, table_name, cols, rows)
+result = str(etree.tostring(doc, pretty_print=True), 'utf-8')
+print(result)
+with open('new_book_xml.xml', 'w') as ouf:
+    ouf.write('<?xml version="1.0"?>'+'\n')
+    ouf.write(result)
